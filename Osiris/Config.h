0c9f4a72c8ccdf0c04bc10073e6af6452110a5df
playday3008 <titanv3585@gmail.com>
2020-05-17 16:11:00 +0200

    Revert "Fix: Slow Walk"
    This reverts commit af2e2102861cd6f7f2b08e70216eb5c671325762.

--- Osiris/Config.h
+++ Osiris/Config.h
@@ -111,7 +111,7 @@ public:
         bool LBYBreaker{ false };
         float LBYAngle{ 0.0f };
         bool Slowwalk{ false };
-        float slowwalkspeed{ 0.0f };
+        int slowwalkspeed{ 0 };
         int slowwalkkey{ 0 };
     } antiAim;
 
