6b47c554061a28943af7e7ad53666428e491d950
PlayDay <18056374+playday3008@users.noreply.github.com>
2021-04-15 05:34:36 +0200

    Update: Nightmode

--- Osiris/Hacks/Visuals.cpp
+++ Osiris/Hacks/Visuals.cpp
@@ -599,23 +599,18 @@ void Visuals::noBloom() noexcept
     bloomCvar->setValue(config->visuals.noBloom ? 1 : 0);
 }
 
-void Visuals::NightMode()noexcept
+void Visuals::NightMode() noexcept
 {
-    static std::string old_Skyname = "";
     static bool OldNightmode;
-    static int OldSky;
     if (!interfaces->engine->isInGame() || !interfaces->engine->isConnected() || !localPlayer || !localPlayer->isAlive()) {
-        old_Skyname = "";
         OldNightmode = false;
-        OldSky = 0;
         return;
     }
 
-    static ConVar* r_DrawSpecificStaticProp;
-
     if (OldNightmode != config->visuals.nightMode)
     {
-        r_DrawSpecificStaticProp = interfaces->cvar->findVar("r_DrawSpecificStaticProp");
+	    static ConVar * r_DrawSpecificStaticProp;
+	    r_DrawSpecificStaticProp = interfaces->cvar->findVar("r_DrawSpecificStaticProp");
         r_DrawSpecificStaticProp->setValue(0);
 
         for (auto i = interfaces->materialSystem->firstMaterial(); i != interfaces->materialSystem->invalidMaterial(); i = interfaces->materialSystem->nextMaterial(i))
