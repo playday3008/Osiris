42eccf06d1cc04e345309e7a4d1bf4049b14b865
PlayDay <18056374+playday3008@users.noreply.github.com>
2021-04-15 05:28:47 +0200

    Rewrite: AntiDetection

--- Osiris/AntiDetection.cpp
+++ Osiris/AntiDetection.cpp
@@ -1,53 +1,47 @@
 #include "AntiDetection.h"
 #include <Windows.h>
+#include <winternl.h>
 
 HMODULE GetSelfModuleHandle()
 {
 	MEMORY_BASIC_INFORMATION mbi;
 
-	return ((::VirtualQuery(GetSelfModuleHandle, &mbi, sizeof(mbi)) != 0)
-		? (HMODULE)mbi.AllocationBase : NULL);
+	return ::VirtualQuery(GetSelfModuleHandle, &mbi, sizeof mbi) != 0
+		       ? static_cast<HMODULE>(mbi.AllocationBase) : nullptr;
 }
 
 void HideModule(void* pModule)
 {
-	void* pPEB = nullptr;
+	auto pPEB = reinterpret_cast<PEB*>(__readfsdword(0x30));
 
-	_asm
-	{
-		push eax
-		mov eax, fs: [0x30]
-		mov pPEB, eax
-		pop eax
-	}
-
-	void* pLDR = *((void**)((unsigned char*)pPEB + 0xc));
-	void* pCurrent = *((void**)((unsigned char*)pLDR + 0x0c));
-	void* pNext = pCurrent;
+	auto pLDR = pPEB->Ldr;
+	auto pCurrent = pLDR->Reserved2[1];
+	auto pNext = pCurrent;
 
 	do
 	{
-		void* pNextPoint = *((void**)((unsigned char*)pNext));
-		void* pLastPoint = *((void**)((unsigned char*)pNext + 0x4));
-		void* nBaseAddress = *((void**)((unsigned char*)pNext + 0x18));
+		auto pNextPoint = *reinterpret_cast<void**>(static_cast<unsigned char*>(pNext));
+		auto pLastPoint = *reinterpret_cast<void**>(static_cast<unsigned char*>(pNext) + 0x4);
+		auto nBaseAddress = pPEB->Reserved3[1];
 
 		if (nBaseAddress == pModule)
 		{
-			*((void**)((unsigned char*)pLastPoint)) = pNextPoint;
-			*((void**)((unsigned char*)pNextPoint + 0x4)) = pLastPoint;
+			*reinterpret_cast<void**>(static_cast<unsigned char*>(pLastPoint)) = pNextPoint;
+			*reinterpret_cast<void**>(static_cast<unsigned char*>(pNextPoint) + 0x4) = pLastPoint;
 			pCurrent = pNextPoint;
 		}
 
-		pNext = *((void**)pNext);
+		pNext = *static_cast<void**>(pNext);
 	} while (pCurrent != pNext);
 }
+
 AntiDetection::AntiDetection() {
 	//clear up PE headers.
-	HMODULE hModule = GetSelfModuleHandle();
+	const HMODULE hModule = GetSelfModuleHandle();
 	DWORD dwMemPro;
-	VirtualProtect((void*)hModule, 0x1000, PAGE_EXECUTE_READWRITE, &dwMemPro);
-	memset((void*)hModule, 0, 0x1000);
-	VirtualProtect((void*)hModule, 0x1000, dwMemPro, &dwMemPro);
+	VirtualProtect(hModule, 0x1000, PAGE_EXECUTE_READWRITE, &dwMemPro);
+	memset(hModule, 0, 0x1000);
+	VirtualProtect(hModule, 0x1000, dwMemPro, &dwMemPro);
 	if (IsDebuggerPresent())
 		OutputDebugStringA("CleanUp PEheader Success.\n");
 	HideModule(hModule);
